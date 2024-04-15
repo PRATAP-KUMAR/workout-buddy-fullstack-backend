56d4a770c56e6980d1932412648f8d73328f732d
PRATAP-KUMAR <pratap@fastmail.fm>
2024-04-15 13:16:20 +0530

    backend complete, start frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // express app
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('backend connected, listening on port 4000!!!');
-})
+// connect to db
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
