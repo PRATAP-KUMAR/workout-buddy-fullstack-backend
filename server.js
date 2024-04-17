cbb0124c0077f55acc6da3bda02ae890fcf267ba
PRATAP-KUMAR <pratap@fastmail.fm>
2024-04-17 09:56:15 +0530

    signup logic implemented

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
-
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
+
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express();
 
 // middleware
+app.use(cors())
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +20,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGODB_URI)
