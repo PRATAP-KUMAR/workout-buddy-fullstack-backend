65ddd806e1a4fb2e1245bca9b2f970c7b8560868
PRATAP-KUMAR <pratap@fastmail.fm>
2024-04-19 13:00:12 +0530

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +20,18 @@ const userSchema = new Schema({
 userSchema.statics.signup = async function (email, password) {
     const exists = await this.findOne({ email })
 
+    if (!email || !password) {
+        throw Error("All fields must be filled");
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is not strong");
+    }
+
     if (exists) {
         throw Error('Email already in use');
     }
@@ -30,5 +43,27 @@ userSchema.statics.signup = async function (email, password) {
 
     return user;
 }
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error("All fields must be filled");
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error('Incorrect email');
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect Password');
+    }
+
+    return user;
+}
+
 
 module.exports = mongoose.model('User', userSchema);
